17ac8ff2a4de31905aae0c6faae0f03812e42ed3
saki <>
2025-12-16 11:02:03 +0900

    StartGameメソッドを追加（2枚ずつ配布する処理をメソッド化）

--- GameManager.cs
+++ GameManager.cs
@@ -1,33 +1,49 @@
+/// <summary>
+/// ゲームの進行を管理するクラス
+/// </summary>
 public class GameManager
 {
     private Deck deck;
     private List<Card> playerHand;
     private List<Card> dealerHand;
-    // コンストラクタ
+    /// <summary>
+    /// コンストラクタ:デッキと手札を初期化
+    /// </summary>
     public GameManager()
     {
         deck = new Deck();
         playerHand = new List<Card>();
         dealerHand = new List<Card>();
     }
+    /// <summary>
+    /// 指定の枚数を手札にカードを配る
+    /// </summary>
+    private void DealCards(List<Card> hand, int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            hand.Add(deck.DrawCard());
+        }
+    }
+
+    /// <summary>
+    /// ゲームをスタートさせるメソッド
+    /// </summary>
     public void StartGame()
     {
-    // プレイヤーに2枚
-    playerHand.Add(deck.DrawCard());
-    playerHand.Add(deck.DrawCard());
-    // ディーラーに2枚
-    dealerHand.Add(deck.DrawCard());
-    dealerHand.Add(deck.DrawCard());
-
-    // プレイヤーの手札を表示する
-    Console.WriteLine("プレイヤーの手札");
-    foreach (Card card in  playerHand)
-    {
-        Console.WriteLine(card);
-    }
-    // ディーラーの手札を表示する
-    Console.WriteLine("ディーラーの手札: ");
-    Console.WriteLine(dealerHand[0]);    //1枚目だけ
-    Console.WriteLine("もう1枚は伏せられています");
+        // プレイヤーに2枚
+        DealCards(playerHand, 2);
+        // ディーラーに2枚
+        DealCards(dealerHand, 2);
+        // プレイヤーの手札を表示する
+        Console.WriteLine("プレイヤーの手札");
+        foreach (Card card in  playerHand)
+        {
+            Console.WriteLine(card);
+        }
+        // ディーラーの手札を1枚だけ表示する
+        Console.WriteLine("ディーラーの手札: ");
+        Console.WriteLine(dealerHand[0]);
+        Console.WriteLine("もう1枚は伏せられています");
     }
 }
