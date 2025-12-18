d6df1fdfefcbc136e1c4c3376b514ea95682e535
saki <>
2025-12-18 11:16:42 +0900

    ゲームの進行をテスト

--- Program.cs
+++ Program.cs
@@ -2,21 +2,11 @@
 {
     static void Main(string[] args)
     {
-        Deck deck = new Deck();
-        deck.Shuffle();
+        GameManager game = new GameManager();
 
-        Dealer dealer = new Dealer();
-
-        // 最初にカードを2枚配る
-        dealer.AddCard(deck.DrawCard());
-        dealer.AddCard(deck.DrawCard());
-
-        Console.WriteLine($"ディーラーの点数: {dealer.CalculateHandValue()}");
-
-        dealer.PlayTurn(deck);
-
-        // 終了時の点数17点以上であればOK
-        Console.WriteLine($"ディーラーの点数: {dealer.CalculateHandValue()}");
-        Console.WriteLine("ディーラーのターン終了");
+       game.StartGame();
+       game.PlayerTurn();
+       game.DealerTurn();
+       game.JudgeWinner();
     }
 }
